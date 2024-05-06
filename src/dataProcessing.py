d6107ff4081207c4a62b4885b6a1984d517a7a24
Anirudh Valiveru <anirudhv@mit.edu>
2024-05-06 15:11:30 -0400

    Added tests for mediapipe estimation

--- src/dataProcessing.py
+++ src/dataProcessing.py
@@ -37,7 +37,7 @@ def setup_video_writer(cap, output_filename='output_video.mp4'):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    return cv2.VideoWriter(output_filename, fourcc, fps, (width, height))
+    return cv2.VideoWriter(output_filename, fourcc, fps, (width, height))    
 
 # Process each video frame.
 def process_video(input_video_path, output_video_path):
@@ -57,6 +57,7 @@ def process_video(input_video_path, output_video_path):
         
         # Convert the BGR image to RGB.
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # print(image.shape)
         image.flags.writeable = False
         height, width, channels = image.shape
         
@@ -64,8 +65,8 @@ def process_video(input_video_path, output_video_path):
         results = pose.process(image)
         landmarks.append(results.pose_landmarks)
 
-        if (results.pose_landmarks == None):
-            print (len(landmarks))
+        # if (results.pose_landmarks == None):
+            # print (len(landmarks))
         
         # Draw the pose annotations on the image.
         keypoints = np.zeros((height, width, channels), dtype="uint8")
@@ -81,17 +82,44 @@ def process_video(input_video_path, output_video_path):
     cap.release()
     writer.release()
 
-    print (len(landmarks))
-    print (landmarks[1000])
-    print (type(landmarks))
+    # print(len(landmarks))
+    # print(landmarks[1000])
+    # print(type(landmarks))
     return landmarks
+
+if __name__ == '__main__':
+    # for i in range(9):
+    #     input_image_path = f'../saved_frames/inputs/{i}.jpg'
+    #     output_image_path = f'../saved_frames/outputs/{i}.jpg'
+    #     # Initialize MediaPipe Pose.
+    #     print('initializing mp_pose')
+    #     mp_pose = mp.solutions.pose
+    #     pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=0) # , model_complexity={0,1,2} (fastest to slowest)
+        
+    #     # Read the image from input_image_path and use pose.process to extract landmarks.
+    #     # Then, write the landmarks to a new image using mp.solutions.drawing_utils.draw_landmarks.
+    #     print('reading image')
+    #     image = cv2.imread(input_image_path)
+    #     print(image.shape)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #     image.flags.writeable = False
+        
+    #     height, width, channels = image.shape
+    #     results = pose.process(image)
+    #     keypoints = np.zeros((height, width, channels), dtype="uint8")
+        
+    #     mp_drawing = mp.solutions.drawing_utils
+    #     if results.pose_landmarks:
+    #         mp_drawing.draw_landmarks(keypoints, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            
+    #     cv2.imwrite(output_image_path, keypoints)
+    # danceURL = input('Link your desired dance here: ')
     
-# danceURL = input('Link your desired dance here: ')
-danceURL = 'https://www.youtube.com/watch?v=9TWj9I3CKzg'
-# filename = input('What are you labeling this file? ')
-filename = 'badtameez_dil'
-input_path = f'../data/{filename}.mp4'
-output_path = f'../keypoints/{filename}.mp4'
-download_youtube_video(danceURL, input_path)
-# Example usage:
-landmarks = process_video(input_path, output_path)
+    danceURL = 'https://www.youtube.com/watch?v=9TWj9I3CKzg'
+    # filename = input('What are you labeling this file? ')
+    filename = 'badtameez_dil'
+    input_path = f'../data/{filename}.mp4'
+    output_path = f'../keypoints/{filename}.mp4'
+    download_youtube_video(danceURL, input_path)
+    # Example usage:
+    landmarks = process_video(input_path, output_path)
