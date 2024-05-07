2807a2d7ee794254d882265a3c3216e798e80573
Anirudh Valiveru <anirudhv@mit.edu>
2024-05-07 13:28:11 -0400

    Added helper function that simply computes pos and angles from an rgb frame (output by render, for example)

--- src/utils/pose_utils.py
+++ src/utils/pose_utils.py
@@ -129,14 +129,92 @@ def compute_velocity(p1, p2, p3, FRAME_DIFF, FRAMES_PER_SECOND):
     
     return velocity
 
+'''
+Computes and extracts landmarks from frame (RGB image) using MediaPipe Pose.
+'''
+def compute_pos_angles_from_frame(frame, mediapipe_pose):
+    pos_dict = dict()
+    angles_dict = dict()
+    end_eff_pos_dict = dict()
+    
+    points = mediapipe_pose.process(frame).pose_landmarks.landmark
+    
+    pos_dict["origin"] = [points[0].x, points[0].y, points[0].z]
+    pos_dict["right_shoulder"] = [points[12].x, points[12].y, points[12].z]
+    pos_dict["left_shoulder"] = [points[11].x, points[11].y, points[11].z]
+    pos_dict["right_elbow"] = [points[14].x, points[14].y, points[14].z]
+    pos_dict["left_elbow"] = [points[13].x, points[13].y, points[13].z]
+    pos_dict["right_hip"] = [points[24].x, points[24].y, points[24].z]
+    pos_dict["left_hip"] = [points[23].x, points[23].y, points[23].z]
+    pos_dict["right_knee"] = [points[26].x, points[26].y, points[26].z]
+    pos_dict["left_knee"] = [points[25].x, points[25].y, points[25].z]
+    pos_dict["right_ankle"] = [points[28].x, points[28].y, points[28].z]
+    pos_dict["left_ankle"] = [points[27].x, points[27].y, points[27].z]
+    pos_dict["right_wrist"] = [points[16].x, points[16].y, points[16].z]
+    pos_dict["left_wrist"] = [points[15].x, points[15].y, points[15].z]
+    
+    end_eff_pos_dict["origin"] = [points[0].x, points[0].y, points[0].z]
+    end_eff_pos_dict["right_ankle"] = [points[28].x, points[28].y, points[28].z]
+    end_eff_pos_dict["left_ankle"] = [points[27].x, points[27].y, points[27].z]
+    end_eff_pos_dict["right_wrist"] = [points[16].x, points[16].y, points[16].z]
+    end_eff_pos_dict["left_wrist"] = [points[15].x, points[15].y, points[15].z]
+
+    angles_dict["right_shoulder"] = compute_angle_3d(14, 12, 24, points)
+    angles_dict["left_shoulder"] = compute_angle_3d(13, 11, 23, points)
+    angles_dict["right_elbow"] = compute_angle_3d(16, 14, 12, points)
+    angles_dict["left_elbow"] = compute_angle_3d(15, 13, 11, points)
+    angles_dict["right_hip"] = compute_angle_3d(12, 24, 26, points)
+    angles_dict["left_hip"] = compute_angle_3d(11, 23, 25, points)
+    angles_dict["right_knee"] = compute_angle_3d(24, 26, 28, points)
+    angles_dict["left_knee"] = compute_angle_3d(23, 25, 27, points)
+    
+    # Find the mean of all points that are values in pos_dict
+    center_of_mass = np.mean([pos for pos in pos_dict.values()], axis=0)
+    
+    return {
+        'pos': pos_dict, 
+        'angle': angles_dict, 
+        'end_eff_pos': end_eff_pos_dict,
+        'center_of_mass': center_of_mass
+    }
+
+def compute_velocities_between_poses(prev_pos, curr_pos, next_pos, FRAME_DIFF, FRAMES_PER_SECOND):
+    prev_pos_dict = prev_pos['pos']
+    prev_angles_dict = prev_pos['angle']
+    curr_pos_dict = curr_pos['pos']
+    curr_angles_dict = curr_pos['angle']
+    next_pos_dict = next_pos['pos']
+    next_angles_dict = next_pos['angle']
+    
+    velocities_dict = dict()
+    angle_velocities_dict = dict()
+    
+    for key in prev_pos_dict.keys():
+        prev_pos = prev_pos_dict[key]
+        curr_pos = curr_pos_dict[key]
+        next_pos = next_pos_dict[key]
+        velocities_dict[key] = compute_velocity(prev_pos, curr_pos, next_pos, FRAME_DIFF, FRAMES_PER_SECOND)
+    
+    for key in prev_angles_dict.keys():
+        prev_angles = prev_angles_dict[key]
+        curr_angles = curr_angles_dict[key]
+        next_angles = next_angles_dict[key]
+        angle_velocities_dict[key] = compute_velocity(prev_angles, curr_angles, next_angles, FRAME_DIFF, FRAMES_PER_SECOND)
+    
+    return {
+        'velocity': velocities_dict,
+        'angle_velocity': angle_velocities_dict
+    }
+
 def compute_pos_angles(landmarks, FRAME_DIFF, FRAMES_PER_SECOND):
+    
+    
+    
     poses = []
     end_eff_poses = []
     angles = []
     cen_mass = []
 
-    # print('landmarks', landmarks)
-    # print('length', len(landmarks))
     for data_point in landmarks:
         pos_dict = dict()
         end_eff_pos_dict = dict()
@@ -157,7 +235,7 @@ def compute_pos_angles(landmarks, FRAME_DIFF, FRAMES_PER_SECOND):
         pos_dict["left_ankle"] = [points[27].x, points[27].y, points[27].z]
         pos_dict["right_wrist"] = [points[16].x, points[16].y, points[16].z]
         pos_dict["left_wrist"] = [points[15].x, points[15].y, points[15].z]
-
+        
         end_eff_pos_dict["origin"] = [points[0].x, points[0].y, points[0].z]
         end_eff_pos_dict["right_ankle"] = [points[28].x, points[28].y, points[28].z]
         end_eff_pos_dict["left_ankle"] = [points[27].x, points[27].y, points[27].z]
@@ -172,10 +250,9 @@ def compute_pos_angles(landmarks, FRAME_DIFF, FRAMES_PER_SECOND):
         angles_dict["left_hip"] = compute_angle_3d(11, 23, 25, points)
         angles_dict["right_knee"] = compute_angle_3d(24, 26, 28, points)
         angles_dict["left_knee"] = compute_angle_3d(23, 25, 27, points)
-
-        coords = np.array(pos_dict.values())
-        center_of_mass = np.mean(coords, axis=0)
-
+        
+        # Find the mean of all points that are values in pos_dict
+        center_of_mass = np.mean([pos for pos in pos_dict.values()], axis=0)
         cen_mass.append(center_of_mass)
 
         poses.append(pos_dict)
@@ -220,3 +297,17 @@ def compute_pos_angles(landmarks, FRAME_DIFF, FRAMES_PER_SECOND):
             'center_of_mass': center_of_mass
         } for pos, angle, velocity, angle_velocity, center_of_mass in zip(end_eff_poses, angles, velocities, angle_velocities, cen_mass)
     ]
+
+if __name__ == '__main__':
+    # Initialize MediaPipe Pose
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+
+    # Read an image from a file path
+    image_path = '../../saved_frames/inputs/1.jpg'
+    image = cv2.imread(image_path)
+
+    # Convert the color space from BGR to RGB
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = compute_pos_angles_from_frame(image_rgb, pose)
+    print(results)
